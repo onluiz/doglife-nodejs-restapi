98ea05a722d931478a2bd60cbaa9f8cf11515372
onluiz <luiz.alberto1723@gmail.com>
2017-09-21 13:37:02 -0300

    adicionando opção de pesquisa de dogs por id

--- routes/dog.js
+++ routes/dog.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
     var dog = require('../service/dog');
 
+    app.route('/dogs/:dogId')
+        .get(dog.findById);
+
     app.route('/dogs')
         .get(dog.listAll)
         .post(dog.create);
--- service/dog.js
+++ service/dog.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose');
 Dog = mongoose.model('Dog');
 
+exports.findById = function(req, res) {
+    Dog.findById(req.params.dogId, function(err, dog) {
+        if (err)
+            res.send(err);
+        res.json(dog);
+    });
+}
+
 exports.listAll = function(req, res) {
     Dog
         .find({})
